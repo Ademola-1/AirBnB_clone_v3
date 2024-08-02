ab7af746291a8896e74049a12e6ad434caa59303
bugfix.exe <bugfix.exe@gmail.com>
2024-08-02 13:03:03 +0100

    a solution to problem 5 not found, added a return in JSON format for routes that are not included

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """This is the entry point for managing all blueprints"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from flask import g
 
 # A register of the main flask app
 app = Flask(__name__)
 # A register of a blueprint the main flask app.
 app.register_blueprint(app_views)
 
+@app.errorhandler(404)
+def not_found_page(error):
+    return jsonify({"error": "Not found"}), 404
 
 @app.teardown_appcontext
 def tear_down(exception):
